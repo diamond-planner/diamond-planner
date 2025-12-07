a0b0e36d0bbd1cf91b6e4d2a453c29cf364a73fe
David Battefeld <post@davidbattefeld.de>
2025-12-07 23:26:25 +0100

    feat: add main function, update Go deps

---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"log"
+	"os"
+	_ "time/tzdata"
+
+	"github.com/diamond-planner/diamond-planner/bsm"
+	"github.com/diamond-planner/diamond-planner/dp"
+	"github.com/subosito/gotenv"
+)
+
+// / Loads environment.
+//   - Locally: from .env file
+//   - Production: from container env
+func init() {
+	_ = gotenv.Load()
+
+	if os.Getenv("APPLICATION_CONTEXT") == "" {
+		log.Fatal("APPLICATION_CONTEXT not set, error loading environment variables")
+	}
+
+	if os.Getenv("BSM_API_KEY") == "" || os.Getenv("BSM_API_HOST") == "" {
+		log.Fatal("BSM API key or host not set, error loading environment variables")
+	}
+}
+
+func main() {
+	client := bsm.NewAPIClient()
+	app := dp.NewDiamondPlanner(client)
+
+	if err := app.Start(); err != nil {
+		log.Fatal(err)
+	}
+}
