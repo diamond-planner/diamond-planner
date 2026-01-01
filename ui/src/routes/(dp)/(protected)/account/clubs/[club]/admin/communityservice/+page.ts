5ba46645793d1a88b5a3587e7c386e19dd436aa5
David Battefeld <post@davidbattefeld.de>
2026-01-01 17:42:51 +0100

    feat: load admin data per season

--- ui/src/routes/(dp)/(protected)/account/clubs/[club]/admin/communityservice/+page.ts
+++ ui/src/routes/(dp)/(protected)/account/clubs/[club]/admin/communityservice/+page.ts
@@ -2,9 +2,21 @@ import type {PageLoad} from './$types';
 import {client} from "$lib/dp/client.svelte.ts";
 import type {ClubCommunityServiceRow} from "$lib/dp/types/ClubCommunityServiceRow.ts";
 
-export const load: PageLoad = (async ({fetch, params, depends, parent}) => {
+export const load: PageLoad = (async ({fetch, params, url, depends, parent}) => {
+  type Query = {
+    season?: string;
+  }
+  const query: Query = {};
+
+  let season = url.searchParams.get("season");
+  if (!season) {
+    season = new Date().getFullYear().toString();
+  }
+  query.season = season;
+
   const rows = client.send<ClubCommunityServiceRow>(`/api/clubs/${params.club}/admin/communityservice`, {
     fetch: fetch,
+    query: query,
   });
   const parentData = await parent();
 
